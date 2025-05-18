e95c3571d2d98405d12e049cc447042fedae8c21
Thalisu <thalisondmcc@gmail.com>
2025-05-17 23:09:17 -0300

    chore: update types

--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,23 @@
+import type { HTMLAttributes } from "react";
+
+export type TSupportedLocales = "pt-BR" | "en";
+
 export interface ICalendarDay {
   day: number | null;
   disabled: boolean;
+}
+
+export interface IAppointmentDataContext {
+  label?: { calendar: string; hourPicker: string };
+  locale?: TSupportedLocales;
+  duration?: number;
+  minHour?: number;
+  maxHour?: number;
+  onSchedule: (startDate: Date, endDate: Date) => Promise<void>;
+  calendarProps?: HTMLAttributes<HTMLDivElement>;
+  hourPickerProps?: HTMLAttributes<HTMLDivElement>;
+}
+
+export interface IAppointmentData extends Required<IAppointmentDataContext> {
+  interval: number;
 }
